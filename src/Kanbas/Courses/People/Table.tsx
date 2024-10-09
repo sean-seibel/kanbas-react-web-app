bb8c81e218b27bcdf47a42d058c85116ba217699
sean-seibel <seibel.s@northeastern.edu>
2024-10-08 22:24:19 -0400

    people

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -26,7 +26,42 @@ export default function PeopleTable() {
             <td className="wd-last-activity">2020-10-01</td>
             <td className="wd-total-activity">10:21:32</td>{" "}
           </tr>
-          {/* Add at least 3 more users such as Bruce Wayne, Steve Rogers, and Natasha Romanoff */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Barack</span>{" "}
+              <span className="wd-last-name">Obama</span>
+            </td>
+            <td className="wd-login-id">001234562S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-03</td>
+            <td className="wd-total-activity">9:22:07</td>{" "}
+          </tr>
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Donald</span>{" "}
+              <span className="wd-last-name">Trump</span>
+            </td>
+            <td className="wd-login-id">001234563S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">TA</td>
+            <td className="wd-last-activity">2020-10-04</td>
+            <td className="wd-total-activity">17:01:02</td>{" "}
+          </tr>
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Joe</span>{" "}
+              <span className="wd-last-name">Biden</span>
+            </td>
+            <td className="wd-login-id">001234564S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-03</td>
+            <td className="wd-total-activity">10:21:32</td>{" "}
+          </tr>
         </tbody>
       </table>
     </div>
