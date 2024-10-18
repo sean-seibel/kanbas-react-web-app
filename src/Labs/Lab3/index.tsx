11ae6dea3c47ba66b856e05811a552102503200b
sean-seibel <seibel.s@northeastern.edu>
2024-10-18 15:09:19 -0400

    lab 3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,77 @@
+import ArrowFunctions from "./Functions/ArrowFunctions";
+import BooleanVariables from "./Basics/BooleanVariables";
+import ConditionalOutputIfElse from "./Basics/ConditionalOutputIfElse";
+import ConditionalOutputInline from "./Basics/ConditionalOutputInline";
+import IfElse from "./Basics/IfElse";
+import ImpliedReturn from "./Functions/ImpliedReturn";
+import LegacyFunctions from "./Functions/LegacyFunctions";
+import TemplateLiterals from "./Functions/TemplateLiterals";
+import TernaryOperator from "./Basics/TernaryOperator";
+import VariablesAndConstants from "./Basics/VariablesAndConstants";
+import VariableTypes from "./Basics/VariableTypes";
+import ArrayIndexAndLength from "./Structures/ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./Structures/AddingAndRemovingToFromArrays";
+import ForLoops from "./Structures/ForLoops";
+import MapFunction from "./Structures/MapFunction";
+import FindFunction from "./Structures/FindFunction";
+import FindIndex from "./Structures/FindIndex";
+import FilterFunction from "./Structures/FilterFunction";
+import House from "./Structures/House";
+import TodoList from "./Structures/todos/TodoList";
+import Spreading from "./Structures/Spreading";
+import Destructing from "./Structures/Destructing";
+import FunctionDestructing from "./Structures/FunctionDestructing";
+import DestructingImports from "./Structures/DestructingImports";
+import Classes from "./DynamicStyling/Classes";
+import Styles from "./DynamicStyling/Styles";
+import Add from "./Parameterizing/Add";
+import Square from "./Parameterizing/Square";
+import Highlight from "./Parameterizing/Highlight";
+import PathParameters from "./Parameterizing/PathParameters";
+
 export default function Lab3() {
+  console.log("Hello World!");
   return (
-    <div>
-      <h2>Lab 3</h2>
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <h3>JavaScript</h3>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <House />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione
+        eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo
+        excepturi consectetur. Modi omnis minus sequi maiores, provident
+        voluptates.
+      </Highlight>
+      <PathParameters />
     </div>
   );
 }
