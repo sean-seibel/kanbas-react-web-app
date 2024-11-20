9ad389dcea2defec65ccf84ddd42339747e8956f
sean-seibel <seibel.s@northeastern.edu>
2024-11-19 20:12:06 -0500

    lab 5

--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -26,3 +26,15 @@ export const createTodo = async () => {
   const response = await axios.get(`${TODOS_API}/create`);
   return response.data;
 };
+export const postTodo = async (todo: any) => {
+  const response = await axios.post(`${TODOS_API}`, todo);
+  return response.data;
+};
+export const deleteTodo = async (todo: any) => {
+  const response = await axios.delete(`${TODOS_API}/${todo.id}`);
+  return response.data;
+};
+export const updateTodo = async (todo: any) => {
+  const response = await axios.put(`${TODOS_API}/${todo.id}`, todo);
+  return response.data;
+};
